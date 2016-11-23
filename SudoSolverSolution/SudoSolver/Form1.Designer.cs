1e110c5ab3ffeac1fc948a70d6b37259ee7480e6
calebmauer <calebmauer@gmail.com>
2016-11-23 18:18:51 -0500

    Commit of the original version from 2011
    This is the original code and was written in 2011. It supports variable
    board size and will solve puzzles first with logic, then when that stops
    making progress tries every option with a simple depth first search.
    Doesn't have loading and saving of puzzles.

---
+++ SudoSolverSolution/SudoSolver/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace SudoSolver
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbSize = new System.Windows.Forms.ComboBox();
+            this.btnNewBoard = new System.Windows.Forms.Button();
+            this.btnTestBoard = new System.Windows.Forms.Button();
+            this.btnSolve = new System.Windows.Forms.Button();
+            this.btnCheckProgress = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbSize
+            // 
+            this.cmbSize.FormattingEnabled = true;
+            this.cmbSize.Items.AddRange(new object[] {
+            "3",
+            "4",
+            "5",
+            "6",
+            "7"});
+            this.cmbSize.Location = new System.Drawing.Point(44, 48);
+            this.cmbSize.Name = "cmbSize";
+            this.cmbSize.Size = new System.Drawing.Size(121, 21);
+            this.cmbSize.TabIndex = 0;
+            this.cmbSize.Text = "3";
+            // 
+            // btnNewBoard
+            // 
+            this.btnNewBoard.Location = new System.Drawing.Point(185, 45);
+            this.btnNewBoard.Name = "btnNewBoard";
+            this.btnNewBoard.Size = new System.Drawing.Size(89, 23);
+            this.btnNewBoard.TabIndex = 1;
+            this.btnNewBoard.Text = "btnNewBoard";
+            this.btnNewBoard.UseVisualStyleBackColor = true;
+            this.btnNewBoard.Click += new System.EventHandler(this.btnNewBoard_Click);
+            // 
+            // btnTestBoard
+            // 
+            this.btnTestBoard.Location = new System.Drawing.Point(185, 16);
+            this.btnTestBoard.Name = "btnTestBoard";
+            this.btnTestBoard.Size = new System.Drawing.Size(75, 23);
+            this.btnTestBoard.TabIndex = 2;
+            this.btnTestBoard.Text = "Test Board";
+            this.btnTestBoard.UseVisualStyleBackColor = true;
+            this.btnTestBoard.Click += new System.EventHandler(this.btnTestBoard_Click_1);
+            // 
+            // btnSolve
+            // 
+            this.btnSolve.Location = new System.Drawing.Point(291, 45);
+            this.btnSolve.Name = "btnSolve";
+            this.btnSolve.Size = new System.Drawing.Size(75, 23);
+            this.btnSolve.TabIndex = 3;
+            this.btnSolve.Text = "Solve";
+            this.btnSolve.UseVisualStyleBackColor = true;
+            this.btnSolve.Click += new System.EventHandler(this.btnSolve_Click_1);
+            // 
+            // btnCheckProgress
+            // 
+            this.btnCheckProgress.Location = new System.Drawing.Point(291, 16);
+            this.btnCheckProgress.Name = "btnCheckProgress";
+            this.btnCheckProgress.Size = new System.Drawing.Size(104, 23);
+            this.btnCheckProgress.TabIndex = 4;
+            this.btnCheckProgress.Text = "Check Progress";
+            this.btnCheckProgress.UseVisualStyleBackColor = true;
+            this.btnCheckProgress.Click += new System.EventHandler(this.btnCheckProgress_Click_1);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(528, 445);
+            this.Controls.Add(this.btnCheckProgress);
+            this.Controls.Add(this.btnSolve);
+            this.Controls.Add(this.btnTestBoard);
+            this.Controls.Add(this.btnNewBoard);
+            this.Controls.Add(this.cmbSize);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbSize;
+        private System.Windows.Forms.Button btnNewBoard;
+        private System.Windows.Forms.Button btnTestBoard;
+        private System.Windows.Forms.Button btnSolve;
+        private System.Windows.Forms.Button btnCheckProgress;
+    }
+}
+
---
+++ SudoSolverSolution/SudoSolver/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SudoSolver
+{
+    public partial class frmMain : Form
+    {
+        public int n;
+        public int m;
+        TextBox[,] boardTextBoxes;
+        Panel boardPanel;
+        bool IsBoardInitialized = false;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void generateBoard()
+        {
+            int txtBoxWidth = 25;
+            int txtBoxHeight = 25;
+            int squarePadding = 5;
+            int startPosX = 35;
+            int startPosY = 100;
+            int panelPadding = 10;
+            const int MAX_TEXT_LENGTH = 2;
+
+            IsBoardInitialized = true;
+
+            if (boardTextBoxes != null)
+            {
+                foreach (TextBox box in boardTextBoxes)
+                {
+                    box.Dispose();
+                }
+            }
+            if (boardPanel != null)
+            {
+                boardPanel.Dispose();
+            }
+            boardPanel = new Panel();
+            boardPanel.Location = new Point(startPosX, startPosY);
+            boardPanel.Size = new Size(panelPadding * 2 + squarePadding * (n - 1) + m * txtBoxWidth,
+                                        panelPadding * 2 + squarePadding * (n - 1) + m * txtBoxHeight);
+            boardPanel.BackColor = Color.PowderBlue;
+            this.Controls.Add(boardPanel);
+            boardTextBoxes = new TextBox[m, m];
+
+            for (int row = 0; row < boardTextBoxes.GetLength(0); row++)
+            {
+                for (int col = 0; col < boardTextBoxes.GetLength(1); col++)
+                {
+                    boardTextBoxes[row, col] = new TextBox();
+                    boardTextBoxes[row, col].Multiline = true;
+                    boardTextBoxes[row, col].Size = new Size(txtBoxWidth, txtBoxHeight);
+
+                    boardTextBoxes[row, col].Location = new Point(
+                                            panelPadding + (txtBoxWidth * col) + (squarePadding * (col / n))
+                                            , panelPadding + (txtBoxHeight * row) + (squarePadding * (row / n)));
+                    boardTextBoxes[row, col].MaxLength = MAX_TEXT_LENGTH;
+                    boardTextBoxes[row, col].TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+
+                    boardPanel.Controls.Add(boardTextBoxes[row, col]);
+                }
+            }
+        }
+
+        int[,] makeIntFromBoard(SudoSqr[,] board)
+        {
+            int[,] returnArray = new int[board.GetLength(0), board.GetLength(1)];
+
+            for (int row = 0; row < board.GetLength(0); row++)
+            {
+                for (int col = 0; col < board.GetLength(1); col++)
+                {
+                    returnArray[row, col] = board[row, col].getValue();
+                }
+            }
+
+            return returnArray;
+        }
+        private SudoBoard makeBoardFromText()
+        {
+            SudoSqr[,] newBoard = new SudoSqr[m, m];
+            for (int row = 0; row < m; row++)
+            {
+                for (int col = 0; col < m; col++)
+                {
+                    if (boardTextBoxes[row, col].Text != "")
+                        newBoard[row, col] = new SudoSqr(n, Convert.ToInt32(boardTextBoxes[row, col].Text));
+                    else newBoard[row, col] = new SudoSqr(n, SudoSqr.UNSET);
+                }
+            }
+            return new SudoBoard(n, newBoard);
+        }
+
+        private String[,] makeStringArray(int[,] numArray)
+        {
+            String[,] stringArray = new String[numArray.GetLength(0), numArray.GetLength(1)];
+
+            for (int row = 0; row < numArray.GetLength(0); row++)
+            {
+                for (int col = 0; col < numArray.GetLength(1); col++)
+                {
+                    stringArray[row, col] = numArray[row, col].ToString();
+                    if (numArray[row, col] == 0)
+                    {
+                        stringArray[row, col] = "";
+                    }
+                }
+            }
+
+            return stringArray;
+        }
+        private void setTextFromStringArray(String[,] board)
+        {
+            for (int row = 0; row < board.GetLength(0); row++)
+            {
+                for (int col = 0; col < board.GetLength(1); col++)
+                {
+                    boardTextBoxes[row, col].Text = board[row, col];
+                    if (board[row, col] != "")
+                    {
+                        boardTextBoxes[row, col].Enabled = false;
+                        boardTextBoxes[row, col].Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                        boardTextBoxes[row, col].ForeColor = SystemColors.ControlText;
+                    }
+                }
+            }
+        }
+
+        private void btnNewBoard_Click(object sender, EventArgs e)
+        {
+            n = Convert.ToInt32(cmbSize.Text);
+            m = n * n;
+            generateBoard();
+            btnSolve.Enabled = true;
+        }
+
+        private void btnTestBoard_Click_1(object sender, EventArgs e)
+        {
+            String[,] testBoard = new String[9, 9] {{"", "2", "7", "", "1", "", "", "", "4"},
+                                          {"", "6", "", "3", "", "8", "", "", ""},
+                                          {"1", "", "3", "7", "4", "", "", "5", ""},
+                                          {"", "", "6", "", "", "", "8", "", "5"},
+                                          {"5", "8", "", "", "7", "", "", "1", "3"},
+                                          {"3", "", "2", "", "", "", "9", "", ""},
+                                          {"", "7", "", "", "5", "4", "6", "", "9"},
+                                          {"", "", "", "8", "", "7", "", "4", ""},
+                                          {"6", "", "", "", "3", "", "1", "8", ""}};
+
+           /* String[,] testBoard = new String[9, 9] {{"", "5", "8", "7", "", "", "", "", "4"},
+                                          {"", "", "", "", "", "", "", "8", "3"},
+                                          {"3", "", "", "9", "8", "", "6", "", "7"},
+                                          {"", "", "", "", "1", "", "", "", ""},
+                                          {"8", "7", "", "", "", "", "", "4", "6"},
+                                          {"", "", "", "", "7", "", "", "", ""},
+                                          {"2", "", "3", "", "4", "1", "", "", "8"},
+                                          {"1", "8", "", "", "", "", "", "", ""},
+                                          {"9", "", "", "", "", "8", "3", "5", ""}};*/
+            n = 3;
+            m = n * n;
+            generateBoard();
+            btnSolve.Enabled = true;
+            setTextFromStringArray(testBoard);
+        }
+
+        private void btnSolve_Click_1(object sender, EventArgs e)
+        {
+            SudoBoard newSolve = makeBoardFromText();
+            bool result = newSolve.solveBoard();
+            SudoSqr[,] newResult = newSolve.board;
+            int[,] intArray = makeIntFromBoard(newResult);
+            setTextFromStringArray(makeStringArray(intArray));
+
+        }
+
+        private void btnCheckProgress_Click_1(object sender, EventArgs e)
+        {
+            SudoBoard board = makeBoardFromText();
+            MessageBox.Show(board.IsSolved().ToString());
+        }
+
+
+    }
+}
